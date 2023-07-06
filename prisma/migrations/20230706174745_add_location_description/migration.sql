6eafdcbfa77c7bd82f57749089b52d8cfe88742a
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-07-06 15:21:07 -0300

    feat: add location description

---
+++ prisma/migrations/20230706174745_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, itaque! Id eum, distinctio laudantium unde corporis doloremque adipisci at sunt dolore? Exercitationem dolores iusto enim iste pariatur sequi. Quisquam, maxime.';
