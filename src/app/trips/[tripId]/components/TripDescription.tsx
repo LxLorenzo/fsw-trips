506acabb8588874669d931a8961b01318936f52a
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-07-06 14:42:43 -0300

    feat: add trip description component

---
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -0,0 +1,18 @@
+import { Trip } from '@prisma/client'
+import React from 'react'
+
+interface TripDescriptionProps {
+  trip: Trip
+}
+const TripDescription = ({ trip }: TripDescriptionProps) => {
+  return (
+    <div className="flex flex-col p-5">
+      <h2 className="text-primaryDarker text-md font-semibold">
+        Sobre a Viagem
+      </h2>
+      <p className="text-primaryDarker text-sm mt-1">{trip.description}</p>
+    </div>
+  )
+}
+
+export default TripDescription
