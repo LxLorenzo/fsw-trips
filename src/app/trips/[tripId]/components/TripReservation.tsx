b9d8e552eb0114485467ece59cf36147748cbd7a
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-07-06 20:38:26 -0300

    feat: add more date validation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -3,13 +3,16 @@
 import Button from '@/app/components/Button'
 import DatePicker from '@/app/components/DatePicker'
 import Input from '@/app/components/Input'
+import { differenceInDays } from 'date-fns'
 import React from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
 interface TripReservationProps {
+  tripId: string
   tripStartDate: Date
   tripEndDate: Date
   maxGuests: number
+  pricePerDay: number
 }
 
 interface TripReservationForm {
@@ -22,19 +25,60 @@ const TripReservation = ({
   tripEndDate,
   tripStartDate,
   maxGuests,
+  pricePerDay,
+  tripId,
 }: TripReservationProps) => {
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = async (data: TripReservationForm) => {
+    const response = await fetch('http://localhost:3000/api/trips/check', {
+      method: 'POST',
+      body: Buffer.from(
+        JSON.stringify({
+          startDate: data.startDate,
+          endDate: data.endDate,
+          tripId,
+        }),
+      ),
+    })
+
+    const res = await response.json()
+    if (res?.error?.code === 'TRIP_ALREADY_RESERVED') {
+      setError('startDate', {
+        type: 'manual',
+        message: 'Esta data já está reservada.',
+      })
+
+      setError('endDate', {
+        type: 'manual',
+        message: 'Esta data já está reservada.',
+      })
+    }
+
+    if (res?.error?.code === 'INVALID_START_DATE') {
+      setError('startDate', {
+        type: 'manual',
+        message: 'Data inválida.',
+      })
+    }
+
+    if (res?.error?.code === 'INVALID_END_DATE') {
+      setError('endDate', {
+        type: 'manual',
+        message: 'Data inválida.',
+      })
+    }
   }
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     control,
     watch,
+    setError,
   } = useForm<TripReservationForm>()
 
   const startDate = watch('startDate')
+  const endDate = watch('endDate')
 
   return (
     <div className="flex flex-col px-5">
@@ -97,11 +141,28 @@ const TripReservation = ({
         errorMessage={errors?.guests?.message}
         placeholder={`Número de hóspedes: ${maxGuests}`}
         className="mt-2.5"
+        onKeyPress={(event) => {
+          const keyCode = event.keyCode || event.which
+          const keyValue = String.fromCharCode(keyCode)
+          const isValid = /^\d+$/.test(keyValue)
+          if (!isValid) {
+            event.preventDefault()
+          }
+        }}
       />
 
       <div className="flex justify-between mt-2.5">
-        <p className="font-medium text-sm text-primaryDarker">Total</p>
-        <p className="font-medium text-sm text-primaryDarker">R$25000</p>
+        <p className="font-medium text-sm text-primaryDarker">
+          Total
+          {startDate && endDate
+            ? ` (${differenceInDays(endDate, startDate)} noites)`
+            : ''}
+        </p>
+        <p className="font-medium text-sm text-primaryDarker">
+          {startDate && endDate
+            ? `R$${differenceInDays(endDate, startDate) * pricePerDay}`
+            : 'R$0'}
+        </p>
       </div>
 
       <div className="pb-10 border-b border-b-graySecondary w-full">
